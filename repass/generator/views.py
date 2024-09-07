0de6e4538beb54b5ba3dea14b1bfb7c068259346
thunderboyk <lakshyajun27@gmail.com>
2024-09-07 19:09:26 +0530

    Add files via upload
    so basically after sweating so much we were able to direct it to the home.html file now after that we also learnt in veiws.py we can add dictionary and call them in the  main page lol thats it finally

--- repass/generator/views.py
+++ repass/generator/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-	return HttpResponse('hi there ')
+	return render(request,'generator/home.html')
--- repass/repass/urls.py
+++ repass/repass/urls.py
@@ -19,8 +19,6 @@ from django.urls import path
 from generator import views
 
 urlpatterns = [
-    path('', views.home),
-
-
+    path('',views.home),
 ]
 
