64d0183ecefa28c805e8fbdbfaf15093497b3126
thunderboyk <lakshyajun27@gmail.com>
2024-09-07 22:51:21 +0530

    Add files via upload
    basically creating a random pass word before we take input from the user

--- repass/generator/views.py
+++ repass/generator/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random 
 # Create your views here.
 # after generator, {'password':'aijemsuvw'} and call in the html file by using 2 {{password}} bracket 
 def home(request):
 	return render(request,'generator/home.html')
 
-
 def password(request):
-	return render(request,'generator/Password.html')
+	
+	charecters= list('abcdefghijklmnopqrstuvwxyz')
+
+	length = 10
+
+	thepassword = ' '
+	for x in range(length):
+		thepassword+= random.choice(charecters)
+
+	return render(request,'generator/Password.html',{'password':thepassword})
+
