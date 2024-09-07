b04eaa33b073948cc52d5b819807646e58bbaf92
thunderboyk <lakshyajun27@gmail.com>
2024-09-07 21:00:35 +0530

    Add files via upload
    so this time in action button in html we first start to put a url  ...... it finds the name  .... self explained in code yes

--- repass/repass/urls.py
+++ repass/repass/urls.py
@@ -21,7 +21,7 @@ from generator import views
 
 urlpatterns = [
     path('',views.home),
-    path('password/',views.password)
+    path('generatedpassword/',views.password,name  = 'passwor')
 
 ]
 
