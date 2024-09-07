fab906e8c8d1cf5b6e63619feb31d4190075e4f8
thunderboyk <lakshyajun27@gmail.com>
2024-09-07 23:07:10 +0530

    Add files via upload
    basically it is done but now we need to make an about page and a place to redirect it back to

--- repass/generator/views.py
+++ repass/generator/views.py
@@ -6,12 +6,35 @@ import random
 def home(request):
 	return render(request,'generator/home.html')
 
+
+
 def password(request):
 	
 	charecters= list('abcdefghijklmnopqrstuvwxyz')
 
-	length = 10
+	if request.GET.get('Uppercase'):
 
+		charecters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWZYZ'))
+
+	
+	if request.GET.get('special'):
+
+		charecters.extend(list('!@#$%^&*()_+'))
+
+	if request.GET.get('numbers'):
+
+		charecters.extend(list('1234567890'))	
+
+
+
+
+
+	length = int(request.GET.get('length',14))
+
+
+	# so 14 is the basic parameter or default length if noting is selescted 
+# basically this code gets input from the user GET and translates it to the html input of length
+	
 	thepassword = ' '
 	for x in range(length):
 		thepassword+= random.choice(charecters)
