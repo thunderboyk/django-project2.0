7c2f2e7518eaaf9ba5cfa17e729edb5ad9ca3d57
thunderboyk <lakshyajun27@gmail.com>
2024-09-08 15:54:30 +0530

    Add files via upload
    basically using bootsstrap for making the site look good

--- repass/repass/wsgi.py
+++ repass/repass/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'repass.settings')
 
 application = get_wsgi_application()
+
+
+
+#used to make the site live 
